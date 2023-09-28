38b67dcd47cfb2b9caf7ad68403d989ac02acce0
Sergiy Kibrik <Sergiy_Kibrik@epam.com>
2023-09-28 09:43:07 +0300

    configs: split boot env for salvator-x and spider
    Boards boot system from different storage, so boot settings must be placed
    in per-board defconfig.
    
    Signed-off-by: Sergiy Kibrik <Sergiy_Kibrik@epam.com>

--- include/configs/salvator-x-xen.h
+++ include/configs/salvator-x-xen.h
@@ -36,14 +36,7 @@
 #undef CONFIG_EXTRA_ENV_SETTINGS
 #define CONFIG_EXTRA_ENV_SETTINGS	\
         "fdt_high=0xffffffffffffffff\0" \
-        "initrd_high=0xffffffffffffffff\0" \
-	"bootargs=root=/dev/sda4 ro rootwait console=hvc0 "\
-	"clk_ignore_unused pci=pcie_bus_perf vardir.disk=/dev/sda5 "\
-	"opendisk.target=/dev/sda6 opendisk.pkcs11=optee "\
-	"aosupdate.disk=/dev/aosvg/workdirs aosupdate.path=um/update_rootfs\0"\
-	"bootcmd=scsi scan; ext4load scsi 0:1 0x44001000 linux-domd; "\
-	"ext4load scsi 0:1 0x46000000 initramfs-domd; "\
-	"booti 0x44001000 0x46000000:0x${filesize} 0x48000000\0"
+        "initrd_high=0xffffffffffffffff\0"
 /* Ethernet RAVB */
 #define CONFIG_BITBANGMII_MULTI
 
